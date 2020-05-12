2fa12434161e11e6dbc4a81c608c77cfd4f503d7
cirrux888 <quinn.wong@cirruxsolutions.com>
2020-05-12 13:24:03 -0700

    Added mat-card-title, content, actions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, MatCardModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
